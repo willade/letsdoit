7328fcb059cc14bc514a9bdb98f1b825fab734ea
willade <williamadetunji@yahoo.com>
2017-08-25 16:21:52 +0100

    added

--- letsdoit/process.php
+++ letsdoit/process.php
@@ -1,27 +1,28 @@
 <?php
-$host = "localhost";
-$username = "intern";
-$password = "@hng.intern1";
-$db = "hng";
+ if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $email = $_GET['email'] ;
+    $body = $_GET['message'] ;
+    $subject= $_GET['subject'] ;
+ }
+ $host = "localhost";
+ $username = "intern";
+  $password = "@hng.intern1";
+ $db = "hng";
 // Create connection
-$connection = mysql_connect($host, $username, $password);
-$db =  mysql_select_db($db);
+ $connection = mysql_connect($host, $username, $password);
+ $db =  mysql_select_db($db);
 // Check connection
-if (!$connection) {
+ if (!$connection) {
     die("Connection failed");
-}
+ }
      $query = "SELECT * FROM password";
      mysql_query($query) or die('Error querying database.');
      $result = mysql_query($query);
      $row = mysql_fetch_array($result);
      $password = $row['1'];
-
-        $email = $_REQUEST['email'] ;
-        $body = $_REQUEST['message'] ;
-        $subject= $_REQUEST['subject'] ;
     
-  header("../sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=williamadetunji@yahoo.com");
+  header("location:../sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=williamadetunji@yahoo.com");
 
-  mail($emailID, $subject, $body, header );
+  mail($email, $subject, $body, header );
   echo "<h4>Thank you, Got your message.</h4>";
 ?>
